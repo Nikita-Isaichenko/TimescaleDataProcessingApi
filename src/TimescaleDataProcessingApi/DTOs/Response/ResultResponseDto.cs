66c42aa5a6e13464505b71252dc0d40ab322b410
Nikita Isaichenko <nik_isaichenko@vk.com>
2026-08-20 17:22:17 +0700

    feature(ResultResponseDto.cs): add Dto for response result

---
+++ src/TimescaleDataProcessingApi/DTOs/Response/ResultResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimescaleDataProcessingApi.DTOs.Response
+{
+    public class ResultResponseDto
+    {
+        public string FileName { get; set; } = string.Empty;
+
+        public double DeltaTimeSeconds { get; set; }
+
+        public DateTime FirstOperationStart { get; set; }
+
+        public double AvgExecutionTime { get; set; }
+
+        public double AvgValue { get; set; }
+
+        public double MedianValue { get; set; }
+
+        public double MaxValue { get; set; }
+
+        public double MinValue { get; set; }
+    }
+}
