b37f2cd5327cc1eb4bb9486743a82fcb02a3364f
Nikita Isaichenko <nik_isaichenko@vk.com>
2026-08-20 17:54:20 +0700

    feature(ValueResponseDto.cs): add Dto for ValueEntry

---
+++ src/TimescaleDataProcessingApi/DTOs/Response/ValueResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimescaleDataProcessingApi.DTOs.Response
+{
+    public class ValueResponseDto
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double ExecutionTime { get; set; }
+
+        public double Value { get; set; }
+
+        public string FileName { get; set; } = string.Empty;
+    }
+}
