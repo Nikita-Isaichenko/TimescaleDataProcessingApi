5eccd4ae59a7f6b065ad82758db1fb447003e4e7
Nikita Isaichenko <nik_isaichenko@vk.com>
2026-08-20 17:15:40 +0700

    feature(FilterDto.cs): add filter Dto

---
+++ src/TimescaleDataProcessingApi/DTOs/FilterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TimescaleDataProcessingApi.DTOs
+{
+    public class FilterDto
+    {
+        public string? FileName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public double? MinAvgValue { get; set; }
+        public double? MaxAvgValue { get; set; }
+        public double? MinAvgExecutionTime { get; set; }
+        public double? MaxAvgExecutionTime { get; set; }
+    }
+}
